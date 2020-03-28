9ada8c3bfa216ff9cc964c071a06bbeb55968df7
frontend90 <leon44173@gmail.com>
2020-03-28 16:10:50 +0200

    верстка

---
+++ js/main.js
@@ -0,0 +1,8 @@
+/************    testimonials slick    **************/
+$('.testimonials-slider').slick({
+  dots: true,
+  arrows: false,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true
+});
