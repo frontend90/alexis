2516edfb79fc271fa1ff6beb49d985cad1285a21
frontend90 <leon44173@gmail.com>
2020-03-28 20:53:50 +0200

    верстка

--- js/main.js
+++ js/main.js
@@ -6,3 +6,21 @@ $('.testimonials-slider').slick({
   slidesToScroll: 1,
   autoplay: true
 });
+
+/*********************  плавный скролл    *********/
+$(document).ready(function () {
+  $('a[href^="#"]').click(function () {
+    elementClick = $(this).attr("href");
+    destination = $(elementClick).offset().top;
+    if ($.browser) {
+      $('body').animate({
+        scrollTop: destination
+      }, 1000);
+    } else {
+      $('html').animate({
+        scrollTop: destination
+      }, 1000);
+    }
+    return false;
+  });
+});
